a73c4f129bd3ae1c2d7a2a98d33a7dc2fd586730
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-12-16 15:19:42 +0200

    Update PassGenerator.java

--- PassGenerator.java
+++ PassGenerator.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 import java.util.Random;
 
 public class PassGenerator {
@@ -9,20 +9,22 @@ public class PassGenerator {
 				"N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 				"a","b","c","d","e","f","g","h","i","j","k","l","m",
 				"n","o","p","q","r","s","t","u","v","w","x","y","z"};
+		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("How long you want the password to be ?");
 		
+		int len = scan.nextInt();
 		
-		System.out.println("Your password: "+pass(array));
+		System.out.println("Your password: "+pass(array,len));
 	
 	}
-public static String pass(String[] arr){
+public static String pass(String[] arr,int len){
     
     Random rand = new Random(); 
 		int random;
 		String pass = "";
 		
-		for (int i = 0; i<8; i++) {
+		for (int i = 0; i<len; i++) {
 			random = rand.nextInt(arr.length); 
 			pass+=arr[random];
 			}
